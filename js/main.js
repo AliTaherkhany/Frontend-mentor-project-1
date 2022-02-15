958cadf948a6b124ba040552bad90a191c011271
AliTaherkhany <taherkhany2004@gmail.com>
2022-02-15 21:25:47 +0330

    Header completed :)

--- js/main.js
+++ js/main.js
@@ -13,3 +13,47 @@ function addClassName(item, index) {
         this.classList.add('mouseout')
     })
 }
+
+let nav_product_info = document.querySelector('.nav-product-info');
+document.querySelector('.nav-product-img').style.setProperty('--height', `${nav_product_info.offsetHeight + 10}px`);
+
+let cart = document.getElementById('cart-icon');
+let wrapper = document.querySelector('.wrapper');
+cart.addEventListener('mouseover', show);
+
+function show() {
+    wrapper.classList.remove('hide')
+    wrapper.classList.add('show')
+}
+cart.addEventListener('mouseout', hide);
+
+function hide() {
+    let stat = true;
+    wrapper.addEventListener('mouseover', hold)
+    function hold() {
+        stat = false
+    }
+    wrapper.addEventListener('mouseleave', leave)
+    function leave() {
+        wrapper.classList.remove('show')
+        wrapper.classList.add('hide')
+    }
+    setTimeout(() => {
+        if (stat === true) {
+            wrapper.classList.remove('show')
+            wrapper.classList.add('hide')
+        }
+    }, 500)
+}
+
+let prof = document.querySelector('.profile');
+prof.addEventListener('mouseover', spin);
+function spin() {
+    prof.classList.remove('spinOut')
+    prof.classList.add('spin')
+}
+prof.addEventListener('mouseout', spinOut);
+function spinOut() {
+    prof.classList.remove('spin')
+    prof.classList.add('spinOut')
+}
